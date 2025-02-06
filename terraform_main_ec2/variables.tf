9c5c91600235e7932907d08651d9e85f5a98685e
aravindcheggoju <cheggojuaravind@gmail.com>
2025-02-06 08:39:12 +0530

    Update variables.tf

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -49,13 +49,13 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0d2614eafc1b0e4d2" // Replace with the latest AMI ID for your region
+  default     = "ami-0c50b6f7dc3701ddd" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.medium"
+  default     = "t2.large"
 }
 
 variable "key_name" {
